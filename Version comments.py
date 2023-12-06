2b686d08a36b436bca7ef9ebd0963547fb6198da
SAHAND SABAHI MIAB <SAHAND.SABAHI@GMAIL.COM>
2023-12-06 18:25:36 +0100

    increased time scale of the oscilation

--- Version comments.py
+++ Version comments.py
@@ -1,3 +1,4 @@
 # ad2296be49f19d6ae177063854ae693de1e0ba7b: created a branch for the first phase of project. making shure the model generates SWR that oscilat.
 # 812ca2edcfc442de2f8c036f4d5d09e0f70ba93d: created branch dedicated to get population P to oscilate detached from pop. B.
 # 7d8990b558b137cefe6c1d2f764a5f77a75f0533: oscilation in small time scale.
+# fa6246bcc3fb7a124c921e79e8c113f0c7714c7f: to increase the time sclae of the oscilation, increase adaptation current and decrease background current
