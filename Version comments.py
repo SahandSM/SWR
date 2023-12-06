c1c4cc6b4327f4e01f2f03f1c6bdac5d6f424c22
SAHAND SABAHI MIAB <SAHAND.SABAHI@GMAIL.COM>
2023-12-06 19:37:21 +0100

    version comment summaryversion comment summary

--- Version comments.py
+++ Version comments.py
@@ -2,3 +2,9 @@
 # 812ca2edcfc442de2f8c036f4d5d09e0f70ba93d: created branch dedicated to get population P to oscilate detached from pop. B.
 # 7d8990b558b137cefe6c1d2f764a5f77a75f0533: oscilation in small time scale.
 # fa6246bcc3fb7a124c921e79e8c113f0c7714c7f: to increase the time sclae of the oscilation, increase adaptation current and decrease background current
+
+# f890c385db8d9bff303399c7fb6fc3d92698da22: summary:
+#       threre is a barckground thereshold for the the isolated population P to jumpt to activity.
+#       the threshold doesn't change with the addition of P current. but the jup increases dramaticlly.
+#       when we are close enough to the threshold and above it, adding enough adaptation leads to oscilation.
+#       in presense of P current and in oscilation state, the adaptation curret oscilates between 0 and adaptation increment J_spi_p
