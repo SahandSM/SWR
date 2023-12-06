7d8990b558b137cefe6c1d2f764a5f77a75f0533
SAHAND SABAHI MIAB <SAHAND.SABAHI@GMAIL.COM>
2023-12-06 18:01:52 +0100

    version comments

--- Version comments.py
+++ Version comments.py
@@ -1,1 +1,2 @@
 # ad2296be49f19d6ae177063854ae693de1e0ba7b: created a branch for the first phase of project. making shure the model generates SWR that oscilat.
+# 812ca2edcfc442de2f8c036f4d5d09e0f70ba93d: created branch dedicated to get population P to oscilate detached from pop. B.
